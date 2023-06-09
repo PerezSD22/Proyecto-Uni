35ab363e94f8ffc43abdeedc99b99c402510a647
Samuel Pérez <samuel.d.perez.22@gmail.com>
2023-06-09 10:28:12 -0500

    cambios 9/6/23

--- src/pages/Perfil/Perfil-FristSection/FristSectio.jsx
+++ src/pages/Perfil/Perfil-FristSection/FristSectio.jsx
@@ -1,16 +1,17 @@
-import React from "react";
-import "./FristSection.css"
+    import React from "react";
+    import "./FristSection.css"
 
-const FristSection = () =>{
-    return <div className="frist-section-container">
-        <div className="photo-info-container">
-            <img src="img/user-logo.png" alt="user-logo" />
-            <p>user</p>
+    const FristSection = (props) =>{
+        const {nombre} = props
+        return <div className="frist-section-container">
+            <div className="photo-info-container">
+                <img src="img/user-logo.png" alt="user-logo" />
+                <p>{nombre}</p>
+            </div>
+            <div className="email-container">
+            <p>samuel.d.perez.22@gmail.com</p>
+            </div>
         </div>
-        <div className="email-container">
-          <p>samuel.d.perez.22@gmail.com</p>
-        </div>
-    </div>
-};
+    };
 
-export default FristSection;
+    export default FristSection;
--- src/pages/Perfil/Perfil.jsx
+++ src/pages/Perfil/Perfil.jsx
@@ -1,13 +1,39 @@
-import React from "react";
+import React, { useState, useEffect } from 'react';
 import "./Perfil.css"
 import FristSection from "./Perfil-FristSection/FristSectio";
 import SecundSection from "./Perfil-SecundSection/SecundSection";
-const Perfil = ( ) =>{
+import { getUserProfile } from '../../services/Users/user-api';
+import { useParams } from 'react-router-dom';
 
+const Perfil = ( ) =>{
+    const { userId } = useParams(); // Obtén el ID del usuario de la URL
+    const [user, setUser] = useState([]);
+    
+    useEffect(() => {
+      const fetchUser = async () => {
+        try {
+          const userData = await getUserProfile(userId); // Pasa el ID del usuario a la función getUserProfile()
+          setUser(userData);
+  
+          console.log(userData); // Muestra los datos en la consola
+        } catch (error) {
+          console.error(error);
+        }
+      };
+  
+      fetchUser();
+    }, [userId]);
+  
+    if (!user) {
+      return <div>Cargando perfil de usuario...</div>;
+    }
+  
     return  <div className="perfil-container">
-        <h3 className="Titulo">Datos Generales</h3>
+        <h3 className="Titulo">{userId.apellido}</h3>
         <div className="container">
-         <FristSection/>
+        
+          <FristSection  nombre={user.name} />
+        
         </div>
         <div className="container">
             <SecundSection/>
