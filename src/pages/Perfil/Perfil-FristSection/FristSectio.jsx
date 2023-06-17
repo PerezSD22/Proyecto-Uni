c73bf45821c6d3c9c0d37967af5edd180c381c71
Samuel Pérez <samuel.d.perez.22@gmail.com>
2023-06-17 18:34:53 -0500

    cambios

--- src/pages/Perfil/Perfil-FristSection/FristSectio.jsx
+++ src/pages/Perfil/Perfil-FristSection/FristSectio.jsx
@@ -1,38 +1,60 @@
-    import React from "react";
+    import React, { useContext } from "react";
     import "./FristSection.css"
     import { useState, useEffect } from "react";
-    const FristSection = (props) =>{
-        const {nombre, imagen, apellido} = props
+    import { SessionContext } from "../../../context";
 
+    const FristSection = () =>{
 
-        const fullName = `${nombre} ${apellido}`;
-      
-        const [nameValue, setNameValue] = useState(fullName);
-      
-        const handleNameChange = (event) => {
-          setNameValue(event.target.value);
-        };
-      
-        useEffect(() => {
-          const storedName = localStorage.getItem("name");
-          if (storedName) {
-            setNameValue(storedName);
-          }
-        }, []);
-      
-        useEffect(() => {
-          localStorage.setItem("name", nameValue);
-        }, [nameValue]);
+      const { session} = useContext(SessionContext);
+      // verifico la sesion   
+      const nombre = session ? session.name: "";
+      const apellido = session ? session.lastname: "";
+      const usuario = session ? session.users: "";
+      const email = session ? session.email: "";
+      const imagen = session ? session.image: "";
+      const direccion = session ? session.address: "";
+      const rol = session ? session.rol: "";
+
 
         return <div className="frist-section-container">
             <div className="photo-info-container">
                 <img src={imagen} alt="user-logo" />
-                <input value={nameValue} onChange={handleNameChange} />
+                {/* <input value={} onChange={} /> */}
             </div>
             <div className="email-container">
-            <p>samuel.d.perez.22@gmail.com</p>
+            <p>{nombre}</p>
+            <p>{apellido}</p>
+            <p>{usuario}</p>
+            <p>{email}</p>
+            <p>{direccion}</p>
+            <p>{rol}</p>
             </div>
         </div>
     };
 
     export default FristSection;
+
+
+
+
+    // const {nombre, imagen, apellido} = props props
+
+
+        // const fullName = `${nombre} ${apellido}`;
+      
+        // const [nameValue, setNameValue] = useState(fullName);
+      
+        // const handleNameChange = (event) => {
+        //   // setNameValue(event.target.value);
+        // };
+      
+        // useEffect(() => {
+        //   const storedName = localStorage.getItem("name");
+        //   if (storedName) {
+        //     // setNameValue(storedName);
+        //   }
+        // }, []);
+      
+        // useEffect(() => {
+        //   localStorage.setItem("name", nameValue);
+        // }, [nameValue]);
